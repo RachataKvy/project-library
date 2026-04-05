48727777714ba1a3d2b43f1350f4ce671295e3cc
RachataKvy <198362843+RachataKvy@users.noreply.github.com>
2026-04-05 13:30:41 +0700

    Add remove and toggle read button
    Add the new button into the displayBook loop

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Constructor function to create a new Book object
 function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
     this.title = title;
@@ -5,8 +6,12 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+// Switch the value ex. True => False
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
 
-const myLibrary = [];
+let myLibrary = [];
 
 function addBookToLibrary(title, author, pages, read) {
     // Create a new Book object using the constructor
@@ -17,10 +22,45 @@ function addBookToLibrary(title, author, pages, read) {
 
 function displayBook() {
     let container = document.getElementById("library-container");
+
+    // Empty the container 
     container.innerHTML = "";
+
+    // Loop the array of myLibrary
     for (let book of myLibrary) {
         let contain = document.createElement("div");
-        contain.textContent = `${book.title} - ${book.author} - ${book.pages} - ${book.read ? "Read" : "Not Read"}`;
+
+        // Removing a book
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "Remove";
+        removeBtn.dataset.id = book.id;
+
+        removeBtn.addEventListener("click", (event) => {
+            // Get the id from the button that was clicked
+            const id = event.target.dataset.id;
+            // Filter out the book with that id
+            myLibrary = myLibrary.filter((book) => book.id !== id);
+            displayBook();
+        })
+
+        // Toggle read
+        const toggleRead = document.createElement("button");
+        toggleRead.textContent = "Toggle Read";
+        toggleRead.dataset.id = book.id;
+        toggleRead.addEventListener("click", (event) => {
+            const id = event.target.dataset.id;
+            const book = myLibrary.find((book) => book.id === id);
+            book.toggleRead();
+            displayBook();
+        })
+
+        // Creating the book
+        const bookInfo = document.createElement("p");
+        bookInfo.textContent = `${book.title} - ${book.author} - ${book.pages} - ${book.read ? "Read" : "Not Read"}`;
+
+        contain.appendChild(bookInfo);
+        contain.appendChild(removeBtn);
+        contain.appendChild(toggleRead);
         container.appendChild(contain);
     }
 }
@@ -28,6 +68,7 @@ function displayBook() {
 const form = document.getElementById("input-form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    // Get the value from the html form
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
@@ -37,6 +78,7 @@ form.addEventListener("submit", (event) => {
     addBookToLibrary(title, author, pages, read);
     displayBook();
 })
+
 
 addBookToLibrary("Harry Potter", "J.K. Rowling", 345, true);
 addBookToLibrary("Lord of the Mysterious", "Ai Qianshui de Wuzei", 1145, true);
