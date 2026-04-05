1a9536ea548b0bd4fcc2c261ab3d428dbbd9c642
RachataKvy <198362843+RachataKvy@users.noreply.github.com>
2026-04-05 11:42:50 +0700

    Create a simple html skeleton with input form
    Create functions that take new book and show it on the webpage

--- script.js
+++ script.js
@@ -9,12 +9,35 @@ function Book(title, author, pages, read) {
 const myLibrary = [];
 
 function addBookToLibrary(title, author, pages, read) {
+    // Create a new Book object using the constructor
     const book = new Book(title, author, pages, read);
+    // Stores the new book inside the myLibrary array
     myLibrary.push(book);
 }
 
+function displayBook() {
+    let container = document.getElementById("library-container");
+    container.innerHTML = "";
+    for (let book of myLibrary) {
+        let contain = document.createElement("div");
+        contain.textContent = `${book.title} - ${book.author} - ${book.pages} - ${book.read ? "Read" : "Not Read"}`;
+        container.appendChild(contain);
+    }
+}
+
+const form = document.getElementById("input-form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.querySelector("input[name='read-type']:checked").value === "read";
+
+    // Call addBookToLibrary function with the arguments got from html form
+    addBookToLibrary(title, author, pages, read);
+    displayBook();
+})
+
 addBookToLibrary("Harry Potter", "J.K. Rowling", 345, true);
 addBookToLibrary("Lord of the Mysterious", "Ai Qianshui de Wuzei", 1145, true);
-
-
-console.log(myLibrary)
+displayBook();
